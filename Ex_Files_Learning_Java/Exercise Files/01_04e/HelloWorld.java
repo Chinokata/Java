3421afbe3eeb6efc230f600c49f6b4638a347df9
Chinokata <chinokata@gmail.com>
2022-01-20 11:05:16 +0100

    part1 done

--- Ex_Files_Learning_Java/Exercise Files/01_04e/HelloWorld.java
+++ Ex_Files_Learning_Java/Exercise Files/01_04e/HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World! It's me Kathryn!");
+
+		System.out.println("Hello World! It's me Kate! I love tacos");
 	}
 }
+
+// javac filename.java =» build and run java programs
+
+// IDE (Integrated Development Environment) - to build and run Java
--- Ex_Files_Learning_Java/Exercise Files/02_02b/src/Main.java
+++ Ex_Files_Learning_Java/Exercise Files/02_02b/src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInital = 'K';
+        char studentLastInitial = 'H';
+        boolean hasPerfectAttandence = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInital);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttandence);
     }
 }
+
+// data types => primitives (Boolean, int, double (decimal), char) and reference types ()
